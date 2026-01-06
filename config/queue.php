fd044db44ebbd474f3f03d48ac2fcf1161319f63
Andrey Yurchuk <r1430@tut.by>
2026-01-06 16:20:27 +0300

    feat: add RabbitMQ queue configuration

--- config/queue.php
+++ config/queue.php
@@ -73,6 +73,37 @@ return [
             'after_commit' => false,
         ],
 
+        'rabbitmq' => [
+            'driver' => 'rabbitmq',
+            'queue' => env('RABBITMQ_QUEUE', 'default'),
+
+            'hosts' => [
+                [
+                    'host' => env('RABBITMQ_HOST', '127.0.0.1'),
+                    'port' => env('RABBITMQ_PORT', 5672),
+                    'user' => env('RABBITMQ_USER'),
+                    'password' => env('RABBITMQ_PASSWORD'),
+                    'vhost' => env('RABBITMQ_VHOST', '/'),
+                ],
+            ],
+
+            'options' => [
+                'ssl_options' => [
+                    'cafile' => env('RABBITMQ_SSL_CAFILE', null),
+                    'local_cert' => env('RABBITMQ_SSL_LOCALCERT', null),
+                    'local_key' => env('RABBITMQ_SSL_LOCALKEY', null),
+                    'verify_peer' => env('RABBITMQ_SSL_VERIFY_PEER', true),
+                    'passphrase' => env('RABBITMQ_SSL_PASSPHRASE', null),
+                ],
+                'queue' => [
+                    'job' => VladimirYuldashev\LaravelQueueRabbitMQ\Queue\Jobs\RabbitMQJob::class,
+                ],
+            ],
+
+            'retry_after' => (int) env('RABBITMQ_RETRY_AFTER', 90),
+            'block_for' => null,
+        ],
+
         'deferred' => [
             'driver' => 'deferred',
         ],
